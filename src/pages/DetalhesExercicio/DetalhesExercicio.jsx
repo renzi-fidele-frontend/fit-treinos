53f09f01d6c31d00587cf26be7fa25d5f80965c3
Renzi Fidele <renzifidele2001@gmail.com>
2024-12-14 03:03:48 +0200

    feat: Apanhando os detalhes do respectivo exercício selecionado

--- src/pages/DetalhesExercicio/DetalhesExercicio.jsx
+++ src/pages/DetalhesExercicio/DetalhesExercicio.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { exercisesFetchOptions } from "../../services/ExercicesApi";
+
 const DetalhesExercicio = () => {
-   return <div>DetalhesExercicio</div>;
+   const { id } = useParams();
+
+   const [exercicio, setExercicio] = useState(null);
+
+   const apanharDetalhes = useFetch(`https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`, exercisesFetchOptions, exercicio);
+
+   useEffect(() => {
+      if (!exercicio) {
+         setExercicio(apanharDetalhes.data);
+      }
+   }, [id, apanharDetalhes.data, exercicio]);
+
+   return (
+      <div>
+         {/* TODO: Criar seção de detalhes do exercício */}
+         {/* TODO: Criar seção de vídeos do youtube explicando o respectivo objectivo */}
+         {/* TODO: Renderizar exercícios com o mesmo tipo de equipamento */}
+      </div>
+   );
 };
 export default DetalhesExercicio;
