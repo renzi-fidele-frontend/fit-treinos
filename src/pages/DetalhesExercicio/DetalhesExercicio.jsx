4ce233e8f9c573060d2d2d108ac083e27d247a48
Renzi Fidele <renzifidele2001@gmail.com>
2024-12-28 04:27:25 +0200

    feat: Adidionando a seção dos músculos secundários que serão afectados

--- src/pages/DetalhesExercicio/DetalhesExercicio.jsx
+++ src/pages/DetalhesExercicio/DetalhesExercicio.jsx
@@ -41,14 +41,29 @@ const DetalhesExercicio = () => {
          </Row>
 
          {/* Seção de baixo */}
-         <Row className="mt-5">
-            <Col lg={7}>
+         <Row className="mt-5 text-center">
+            <Col>
                <h2 className="fw-bold mb-4">
                   Parte do corpo que irá se desenvolver: <span className="text-secondary text-capitalize">{exercicio?.bodyPart}</span>
                </h2>
-               <Image className="border-3 shadow-lg border rounded-2" src={fotoDaParteDoCorpo(exercicio?.bodyPart)} alt={exercicio?.bodyPart} fluid />
+               <Image
+                  className="border-3 shadow-lg border rounded-2"
+                  src={fotoDaParteDoCorpo(exercicio?.bodyPart)}
+                  alt={exercicio?.bodyPart}
+                  fluid
+               />
+               <h4 className="mt-5 fw-semibold">Músculos secundários que serão afectados:</h4>
+               <ul className="d-flex gap-5 justify-content-center mt-3">
+                  {exercicio?.secondaryMuscles?.map((v, key) => (
+                     <li className="text-capitalize fs-5 px-3 py-1 text-bg-secondary shadow-sm rounded-3" key={key}>
+                        {v}
+                     </li>
+                  ))}
+               </ul>
+
+               {/* Separador */}
+               <div className="my-5 border border-4 border-bottom rounded-2 shadow-lg dashed"></div>
             </Col>
-            <Col></Col>
          </Row>
          {/* TODO: Criar seção de vídeos do youtube explicando o respectivo objectivo */}
          {/* TODO: Renderizar exercícios com o mesmo tipo de equipamento */}
