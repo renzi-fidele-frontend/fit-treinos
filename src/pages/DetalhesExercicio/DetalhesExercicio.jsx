1e914a89a47607f774607440b7e9760cd741b5f6
Renzi Fidele <renzifidele2001@gmail.com>
2024-12-28 03:09:10 +0200

    feat: Adicionando a estrutura básica da  seção de detalhes do exercício

--- src/pages/DetalhesExercicio/DetalhesExercicio.jsx
+++ src/pages/DetalhesExercicio/DetalhesExercicio.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import { exercisesFetchOptions } from "../../services/ExercicesApi";
+import { Col, Container, Image, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import styles from "./DetalhesExercicio.module.css";
 
 const DetalhesExercicio = () => {
    const { id } = useParams();
@@ -17,11 +19,26 @@ const DetalhesExercicio = () => {
    }, [id, apanharDetalhes.data, exercicio]);
 
    return (
-      <div>
-         {/* TODO: Criar seção de detalhes do exercício */}
+      <Container className="pt-5">
+         <Row>
+            <Col lg={4} className="ps-4 pt-4 me-3 align-items-stretch" id={styles.left}>
+               <Image className="" src={exercicio?.gifUrl} alt={exercicio?.name} fluid />
+            </Col>
+            <Col>
+               <h1 className="fw-bold mb-4">Como praticar o: {exercicio?.name}</h1>
+               <ListGroup>
+                  {exercicio?.instructions?.map((v, key) => (
+                     // TODO: Renderizar as imagens da parte do corpo na lista
+                     <ListGroupItem key={key}>{v}</ListGroupItem>
+                  ))}
+               </ListGroup>
+               <p> {exercicio?.description}</p>
+            </Col>
+         </Row>
+
          {/* TODO: Criar seção de vídeos do youtube explicando o respectivo objectivo */}
          {/* TODO: Renderizar exercícios com o mesmo tipo de equipamento */}
-      </div>
+      </Container>
    );
 };
 export default DetalhesExercicio;
--- src/services/ExercicesApi.js
+++ src/services/ExercicesApi.js
@@ -2,7 +2,7 @@ export const exercisesFetchOptions = {
    method: "GET",
    url: "https://exercisedb.p.rapidapi.com/status",
    headers: {
-      "x-rapidapi-key": import.meta.env.VITE_EXERCISES_API_KEY,
+      "x-rapidapi-key": import.meta.env.VITE_EXERCISES_API_KEY2,
       "x-rapidapi-host": "exercisedb.p.rapidapi.com",
    },
 };
