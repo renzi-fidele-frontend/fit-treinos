158b148c8c7d2f5d542a2858cb61f49e157f792b
Renzi Fidele <renzifidele2001@gmail.com>
2024-12-28 03:51:07 +0200

    feat: Adicionando a seção da parte o corpo que irá se desenvolver a página dos detalhes dos exercícios

--- src/pages/DetalhesExercicio/DetalhesExercicio.jsx
+++ src/pages/DetalhesExercicio/DetalhesExercicio.jsx
@@ -4,6 +4,7 @@ import useFetch from "../../hooks/useFetch";
 import { exercisesFetchOptions } from "../../services/ExercicesApi";
 import { Col, Container, Image, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import styles from "./DetalhesExercicio.module.css";
+import { fotoDaParteDoCorpo } from "../../utils/fotoParteCorpo";
 
 const DetalhesExercicio = () => {
    const { id } = useParams();
@@ -20,12 +21,13 @@ const DetalhesExercicio = () => {
 
    return (
       <Container className="pt-5">
+         {/* Seção inicial */}
          <Row>
             <Col lg={4} className="ps-4 pt-4 me-3 align-items-stretch" id={styles.left}>
                <Image className="" src={exercicio?.gifUrl} alt={exercicio?.name} fluid />
             </Col>
             <Col>
-               <h1 className="fw-bold mb-4">
+               <h1 className="fw-bold mb-4 pt-3">
                   Como praticar o: <span className="text-capitalize text-secondary">{exercicio?.name}</span>
                </h1>
                <ListGroup>
@@ -38,6 +40,16 @@ const DetalhesExercicio = () => {
             </Col>
          </Row>
 
+         {/* Seção de baixo */}
+         <Row className="mt-5">
+            <Col lg={7}>
+               <h2 className="fw-bold mb-4">
+                  Parte do corpo que irá se desenvolver: <span className="text-secondary text-capitalize">{exercicio?.bodyPart}</span>
+               </h2>
+               <Image className="border-3 shadow-lg border rounded-2" src={fotoDaParteDoCorpo(exercicio?.bodyPart)} alt={exercicio?.bodyPart} fluid />
+            </Col>
+            <Col></Col>
+         </Row>
          {/* TODO: Criar seção de vídeos do youtube explicando o respectivo objectivo */}
          {/* TODO: Renderizar exercícios com o mesmo tipo de equipamento */}
       </Container>
