71f791e5d8dda6f1ee6ddafa7a81ed641cc37c6d
Renzi Fidele <renzifidele2001@gmail.com>
2025-01-04 16:03:28 +0200

    feat: Adicionando a utilidade de gerar array

---
+++ src/components/Paginacao/Paginacao.jsx
@@ -0,0 +1,8 @@
+import { Pagination } from "react-bootstrap";
+
+const Paginacao = ({ onPageClick, paginaAtual, totalPaginas }) => {
+   return <Pagination className="">
+    
+   </Pagination>;
+};
+export default Paginacao;
---
+++ src/utils/gerarArray.js
@@ -0,0 +1,4 @@
+export function gerarArray() {
+   // Gerar um array contendo Integers de 1 até o length
+   return Array.from({ length }, (v, i) => i + 1);
+}
