e77a8bd9c7a864bc63bf0b63593b924ee851dcff
Renzi Fidele <renzifidele2001@gmail.com>
2025-01-15 03:16:26 +0200

    feat: Conectando ao cluster do MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,35 @@
 const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
+
+app.use(express.json());
+
+// Permitindo operações no servidor
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+   res.header("Access-Control-Allow-Methods", "*");
+   next();
+});
+
+// Permitindo qualquer domínio de accesar o servidor
+app.use(cors());
 
 app.get("/", (req, res) => {
    res.send("Servidor está rodando");
 });
 
-app.listen(3000, () => {
-   console.log("Servidor rodando na porta 3000");
+// Caso se navegue para uma rota inexistente
+app.use((req, res) => {
+   res.status(404).json({ mensagem: "Essa rota não foi configurada" });
 });
+
+mongoose
+   .connect(process.env.MONGO_URI)
+   .then(() => {
+      app.listen(process.env.PORT, () => console.log("Conectado ao MongoDB"));
+   })
+   .catch((err) => console.error("Erro ao conectar"));
