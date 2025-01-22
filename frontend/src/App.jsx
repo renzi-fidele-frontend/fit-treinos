2223c6eb6e187bdce047126c3a051a2a6d060486
Renzi Fidele <renzifidele2001@gmail.com>
2025-01-22 06:32:18 +0200

    refactor: Separando o roteador em componente individual

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 import { Provider } from "react-redux";
 import store from "./state/store";
-import DetalhesExercicio from "./pages/DetalhesExercicio/DetalhesExercicio";
 import ScrollTop from "./components/ScrollTop/ScrollTop";
+import Footer from "./components/Footer/Footer";
+import Router from "./router/Router";
 
 // Libs
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Exercicios from "./pages/Exercicios/Exercicios";
-import Footer from "./components/Footer/Footer";
-import Cadastro from "./pages/Cadastro/Cadastro";
-import Login from "./pages/Login/Login";
 
 // TODO: No backend, instalar o passport e passport-local para autenticação
 // TODO: Adicionar dashboard de rastreamento do treino
+// TODO: Guardar o usuário logado no local storage
 
 function App() {
    return (
@@ -25,13 +22,7 @@ function App() {
          <BrowserRouter>
             <Header />
             <ScrollTop>
-               <Routes>
-                  <Route exact path="/" element={<Home />} />
-                  <Route path="/exercicio/:id" element={<DetalhesExercicio />} />
-                  <Route path="/exercicios" element={<Exercicios />} />
-                  <Route path="/cadastro" element={<Cadastro />} />
-                  <Route path="/entrar" element={<Login />} />
-               </Routes>
+               <Router />
             </ScrollTop>
             <Footer />
          </BrowserRouter>
---
+++ frontend/src/router/Router.jsx
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import DetalhesExercicio from "../pages/DetalhesExercicio/DetalhesExercicio";
+import Exercicios from "../pages/Exercicios/Exercicios";
+import Cadastro from "../pages/Cadastro/Cadastro";
+import Login from "../pages/Login/Login";
+import { useSelector } from "react-redux";
+
+const Router = () => {
+   const { user } = useSelector((state) => state.auth);
+
+   return (
+      <Routes>
+         <Route exact path="/" element={<Home />} />
+         <Route path="/exercicio/:id" element={<DetalhesExercicio />} />
+         <Route path="/exercicios" element={<Exercicios />} />
+         <Route path="/cadastro" element={!user ? <Cadastro /> : <Navigate to="/" />} />
+         <Route path="/entrar" element={!user ? <Login /> : <Navigate to="/" />} />
+      </Routes>
+   );
+};
+export default Router;
