cd778947541d3dd7d70243d73c716578a4ab91a2
Renzi Fidele <renzifidele2001@gmail.com>
2025-02-18 03:49:22 +0200

    feat: Adicionando a estrutura básica do gráfico das partes do corpo mais treinadas

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -3,73 +3,78 @@ import styles from "./Dashboard.module.css";
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 import { CategoryScale } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import { useSelector } from "react-redux";
 Chart.register(CategoryScale);
 
 const Dashboard = () => {
    // TODO: Renderizar o gráfico de estatísticas de treino semanal
    const diasDaSemana = ["Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"];
-   const dedicacaoSemanal = [120, 60, 150, 50, 76, 30, 90];
+   const dedicacaoSemanal = [150, 60, 200, 50, 76, 30, 90];
+
+   const { categorias: partesDoCorpo } = useSelector((state) => state.exercicios);
+   const treinosPorCategoria = [1, 0, 4, 0, 0, 4, 0, 2, 8, 0];
 
    return (
-      <Container className="h-100">
-         <h2 className="fw-semibold mb-5">Progresso do treinamento</h2>
+      <Container className="h-100 py-5">
+         <h2 className="fw-semibold mb-5 ">Progresso do treinamento</h2>
 
          {/* Primeira linha */}
          <Row>
             <Col md={4}>
-               <div className="border px-3 py-4 shadow-sm rounded-2">
+               <div className="border border-2 px-3 py-4 shadow-sm rounded-2">
                   <div className="d-flex justify-content-between h-100">
                      <h6 id={styles.tit} className="mb-0">
                         Tempo total de treino
                      </h6>
                   </div>
-                  <h5 className="fs-1 fw-bold my-2">02h30min</h5>
-                  <p className="text-secondary fw-medium mb-0">O tempo acumulado praticando exercícios</p>
+                  <h5 className="fs-1 fw-bold my-3">02h30min</h5>
+                  <p className="text-secondary mb-0">O tempo acumulado praticando exercícios</p>
                </div>
             </Col>
             <Col md={4}>
-               <div className="border px-3 py-4 shadow-sm rounded-2">
+               <div className="border border-2 px-3 py-4 shadow-sm rounded-2">
                   <div className="d-flex justify-content-between h-100">
                      <h6 id={styles.tit} className="mb-0">
                         Treinamentos feitos hoje
                      </h6>
                   </div>
-                  <h5 className="fs-1 fw-bold my-2">
+                  <h5 className="fs-1 fw-bold my-3">
                      5.00{" "}
                      <span id={styles.small} className="text-small text-success">
                         (+10%)
                      </span>
                   </h5>
-                  <p className="text-secondary fw-medium mb-0">Número total de treinamentos executados hoje</p>
+                  <p className="text-secondary mb-0">Número total de treinamentos executados hoje</p>
                </div>
             </Col>
             <Col md={4}>
-               <div className="border px-3 py-4 shadow-sm rounded-2">
+               <div className="border border-2 px-3 py-4 shadow-sm rounded-2">
                   <div className="d-flex justify-content-between h-100">
                      <h6 id={styles.tit} className="mb-0">
                         Média do tempo de treino
                      </h6>
                   </div>
-                  <h5 className="fs-1 fw-bold my-2">
+                  <h5 className="fs-1 fw-bold my-3">
                      02min 93s{" "}
                      <span id={styles.small} className="text-small text-success">
                         (+10%)
                      </span>
                   </h5>
-                  <p className="text-secondary fw-medium mb-0">Tempo dedicado ao treinamento por dia</p>
+                  <p className="text-secondary mb-0">Tempo dedicado ao treinamento por dia</p>
                </div>
             </Col>
          </Row>
 
          {/* Segunda linha */}
-         <Row className="mt-4">
+         <Row className="mt-4 mb-4">
             <Col>
-               <div className="border px-3 py-4 shadow-sm rounded-2">
+               <div className="border border-2 px-3 py-4 shadow-sm rounded-2 h-100">
                   <h6 id={styles.tit} className="mb-0">
                      Estatísticas da Dedicação Semanal
                   </h6>
 
-                  <div className="mt-4 mb-3">
+                  <div className="my-4">
                      <Line
                         data={{
                            labels: diasDaSemana,
@@ -82,25 +87,35 @@ const Dashboard = () => {
                                  borderColor: "rgb(135, 142, 163)",
                                  backgroundColor: "rgba(116, 126, 211, 0.5)",
                                  pointBackgroundColor: "#ffffff",
-                                 
                               },
                            ],
                         }}
                         options={{ responsive: true }}
+                        className={styles.chart}
                      />
                   </div>
-                  <p className="text-secondary fw-medium mb-0">* O unidade do tempo de treino está em segundos</p>
+                  <p className="text-secondary small">* A Unidade do tempo de treino está em segundos</p>
+                  <hr className="mt-4" />
+                  <p className="text-secondary mb-0">
+                     <span className="fw-semibold">Dia mais esforçado:</span> Qua
+                  </p>
+                  <p className="text-secondary mb-0">
+                     <span className="fw-semibold">Última seção de treino:</span> 10/02/2025
+                  </p>
                </div>
             </Col>
             <Col>
-               <div className="border px-3 py-4 shadow-sm rounded-2">
+               <div className="border border-2 px-3 py-4 shadow-sm rounded-2 h-100">
                   <h6 id={styles.tit} className="mb-0">
                      Partes do corpo mais treinadas
                   </h6>
+                  <div className="mt-2">
+                     <Pie data={{ labels: partesDoCorpo, datasets: [{ data: treinosPorCategoria }] }} />
+                  </div>
                </div>
             </Col>
             <Col>
-               <div className="border px-3 py-4 shadow-sm rounded-2">
+               <div className="border border-2 px-3 py-4 shadow-sm rounded-2 h-100">
                   <h6 id={styles.tit} className="mb-0">
                      Exercício mais praticado
                   </h6>
