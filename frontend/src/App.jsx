ffe603c111f678cdbe39fbb1f2d1297cd7b79883
Renzi Fidele <renzifidele2001@gmail.com>
2025-02-22 18:31:25 +0200

    feat:  Armazenando dados da api dos exercícios utilizando o redux-persist para reduzir o número requisições

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import { Provider } from "react-redux";
-import store from "./state/store";
 import ScrollTop from "./components/ScrollTop/ScrollTop";
 import Footer from "./components/Footer/Footer";
 import Router from "./router/Router";
-
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./state/store";
 // Libs
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
@@ -15,13 +15,15 @@ import "slick-carousel/slick/slick-theme.css";
 function App() {
    return (
       <Provider store={store}>
-         <BrowserRouter>
-            <Header />
-            <ScrollTop>
-               <Router />
-            </ScrollTop>
-            <Footer />
-         </BrowserRouter>
+         <PersistGate persistor={persistor} loading={null}>
+            <BrowserRouter>
+               <Header />
+               <ScrollTop>
+                  <Router />
+               </ScrollTop>
+               <Footer />
+            </BrowserRouter>
+         </PersistGate>
       </Provider>
    );
 }
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,6 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import exerciciosReducer from "./exercicios/exerciciosSlice";
 import authReducer from "./auth/authSlice";
 import temaReducer from "./theme/themeSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 
-export default configureStore({ reducer: { exercicios: exerciciosReducer, auth: authReducer, tema: temaReducer } });
+// Combinando os reducers
+const rootReducer = combineReducers({ exercicios: exerciciosReducer, auth: authReducer, tema: temaReducer });
+
+const persistConfig = {
+   key: "root",
+   storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({ reducer: persistedReducer });
+export const persistor = persistStore(store);
