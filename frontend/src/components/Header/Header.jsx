3190f063c83deca3cf4340ac9d7ea3f85dbd14a5
Renzi Fidele <renzifidele2001@gmail.com>
2025-03-12 05:57:39 +0200

    feat: Adicionando a nova logo do site

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import styles from "./Header.module.css";
-import logo from "../../assets/logo.jpg";
+import logo from "../../assets/logo.png";
 import MudarTemaBtn from "../ui/MudarTemaBtn";
 import DropDownBtn from "../ui/DropDownBtn";
 import { useSelector } from "react-redux";
