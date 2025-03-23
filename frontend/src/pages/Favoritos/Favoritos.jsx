f42790d56cabaea2dd9975e0dc265e819d382305
Renzi Fidele <renzifidele2001@gmail.com>
2025-03-23 17:26:16 +0200

    feat: Adicionando o loading de esqueleto a página dos exercícios favoritos

--- frontend/src/pages/Favoritos/Favoritos.jsx
+++ frontend/src/pages/Favoritos/Favoritos.jsx
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 import CardExercicio from "../../components/CardExercicio/CardExercicio";
 import noEx from "../../assets/modelo2.webp";
 import BannerTopo from "../../components/BannerTopo/BannerTopo";
+import { gerarArray } from "../../utils/gerarArray";
 
 const Favoritos = () => {
    const { user } = useSelector((state) => state.auth);
@@ -21,30 +22,38 @@ const Favoritos = () => {
             fotoModelo={fotoBanner}
          />
 
-         <Container className="py-5">
+         <div className="py-4 py-md-5 px-sm-5 px-md-0 container-md">
             <Row>
                <Col>
                   <h2 className="fw-semibold mb-4 ">Exercícios favoritos</h2>
                   <hr />
-                  <Row className="mt-2 mb-5 g-4 flex-content-stretch">
-                     {favoritos?.length > 0 ? (
-                        favoritos?.map((v, k) => (
-                           <Col key={k} sm={6} lg={4}>
-                              <CardExercicio titulo={v?.name} id={v?.id} foto={v?.gifUrl} categoria={v?.secondaryMuscles} />
+                  <Row className="mt-2 mb-5 g-4 flex-content-stretch justify-content-center justify-content-sm-start">
+                     {exercicios ? (
+                        favoritos?.length > 0 ? (
+                           favoritos?.map((v, k) => (
+                              <Col key={k} sm={6} lg={4}>
+                                 <CardExercicio titulo={v?.name} id={v?.id} foto={v?.gifUrl} categoria={v?.secondaryMuscles} />
+                              </Col>
+                           ))
+                        ) : (
+                           <Col className="text-center">
+                              <Image src={noEx} />
+                              <Alert className="mt-4" variant="warning">
+                                 Ainda não existem exercícios favoritos.
+                              </Alert>
+                           </Col>
+                        )
+                     ) : (
+                        gerarArray(3).map((v, k) => (
+                           <Col md={4} sm={6} key={k} className="text-center">
+                              <CardExercicio />
                            </Col>
                         ))
-                     ) : (
-                        <Col className="text-center">
-                           <Image src={noEx} />
-                           <Alert className="mt-4" variant="warning">
-                              Ainda não existem exercícios favoritos.
-                           </Alert>
-                        </Col>
                      )}
                   </Row>
                </Col>
             </Row>
-         </Container>
+         </div>
       </>
    );
 };
