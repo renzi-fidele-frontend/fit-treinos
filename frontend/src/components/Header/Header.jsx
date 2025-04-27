e307fb31086ee1e743bcb89a3a5c40c643ed2fdd
Renzi Fidele <renzifidele2001@gmail.com>
2025-04-27 19:09:35 +0200

    feat: Internacionalizando o conteúdo do cabeçalho para PT

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,36 +6,40 @@ import MudarTemaBtn from "../ui/MudarTemaBtn";
 import DropDownBtn from "../ui/DropDownBtn";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
+   const { t } = useTranslation();
+   const { links } = t("header");
    const loc = useLocation();
    const { user } = useSelector((state) => state.auth);
    const [mostrar, setMostrar] = useState(false);
+
    const MyNav = ({ offcanvas }) => (
       <Nav className={`gap-3 fs-5 ${!offcanvas ? "d-none d-lg-flex" : "d-flex"} `} activeKey={loc.pathname}>
          <Nav.Link className={`${offcanvas && "border-bottom pb-4"}`} active={loc.pathname === "/"} as={Link} to={"/"}>
-            <i className="me-2 d-lg-none bi bi-house-fill"></i> Início
+            <i className="me-2 d-lg-none bi bi-house-fill"></i> {links[0]}
          </Nav.Link>
          <Nav.Link className={`${offcanvas && "border-bottom pb-4"}`} active={loc.pathname === "/exercicios"} as={Link} to={"/exercicios"}>
-            <i className="me-2 d-lg-none bi bi-person-arms-up"></i> Exercícios
+            <i className="me-2 d-lg-none bi bi-person-arms-up"></i> {links[1]}
          </Nav.Link>
          {!user && (
             <>
                <Nav.Link className={`${offcanvas && "border-bottom pb-4"}`} active={loc.pathname === "/cadastro"} as={Link} to={"/cadastro"}>
-                  <i className="me-2 d-lg-none bi bi-person-add"></i> Cadastrar
+                  <i className="me-2 d-lg-none bi bi-person-add"></i> {links[2]}
                </Nav.Link>
                <Nav.Link className={`${offcanvas && "border-bottom pb-4"}`} active={loc.pathname === "/entrar"} as={Link} to={"/entrar"}>
-                  <i className="me-2 d-lg-none bi bi-person-fill-lock"></i> Entrar
+                  <i className="me-2 d-lg-none bi bi-person-fill-lock"></i> {links[3]}
                </Nav.Link>
             </>
          )}
          {user && (
             <>
                <Nav.Link className={`${offcanvas && "border-bottom pb-4"}`} active={loc.pathname === "/dashboard"} as={Link} to="/dashboard">
-                  <i className="me-2 d-lg-none bi bi-graph-up-arrow"></i> Progresso do treinamento
+                  <i className="me-2 d-lg-none bi bi-graph-up-arrow"></i> {links[4]}
                </Nav.Link>
                <Nav.Link className={`${offcanvas && "border-bottom pb-4"}`} active={loc.pathname === "/favoritos"} as={Link} to="/favoritos">
-                  <i className="me-2 d-lg-none bi bi-box2-heart-fill"></i> Favoritos
+                  <i className="me-2 d-lg-none bi bi-box2-heart-fill"></i> {links[5]}
                </Nav.Link>
             </>
          )}
