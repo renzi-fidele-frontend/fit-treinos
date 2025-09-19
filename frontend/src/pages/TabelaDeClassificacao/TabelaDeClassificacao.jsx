6873084c8a92ec6ea92bc2fdbe3d50c0e3a6056e
Renzi Fidele <renzifidele2001@gmail.com>
2025-09-19 18:42:29 +0200

    feat: Finalizando a responsividade da tabela de classificações

--- frontend/src/pages/TabelaDeClassificacao/TabelaDeClassificacao.jsx
+++ frontend/src/pages/TabelaDeClassificacao/TabelaDeClassificacao.jsx
@@ -23,7 +23,7 @@ const TabelaDeClassificacao = () => {
          {/* Banner inicial */}
          <BannerTopo
             titulo="Descubra os usuários mais determinados"
-            descricao="Descubra o ranking dos usuários que mais dedicam tempo ao treinamento neste site"
+            descricao="Seja consistente com o treino para poder chegar ao topo"
             fotoModelo={fotoBanner}
          />
          <div className="py-4 py-md-5 px-sm-5 px-md-0 container-md">
@@ -31,49 +31,51 @@ const TabelaDeClassificacao = () => {
                <Col>
                   <h2 className="fw-semibold mb-4">Tabela de classificação</h2>
                   <hr />
-                  <Table striped className="mt-5 fs-5" bordered responsive>
-                     <thead className="fs-6 fst-italic">
-                        <tr>
-                           <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
-                              <i className="bi bi-trophy-fill me-2"></i> Rank
-                           </th>
-                           <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
-                              <i className="bi bi-person-fill me-2"></i> Usuário
-                           </th>
-                           <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
-                              <i className="bi bi-clock-fill me-2"></i> Tempo de treino
-                           </th>
-                           <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
-                              <i className="bi bi-person-arms-up me-2"></i> Treinos realizados
-                           </th>
-                           <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
-                              <i className="bi bi-calendar-check me-2"></i> Cadastrado em
-                           </th>
-                        </tr>
-                     </thead>
-                     <tbody>
-                        {usuarios?.map((v, k) => (
-                           <tr key={k}>
-                              <td className="fst-italic fw-medium">{k + 1}º lugar</td>
-                              <td>
-                                 <div className="d-flex align-items-center gap-3">
-                                    <Image className="rounded p-0" style={{ width: 50, height: 50 }} thumbnail src={v?.foto} />
-                                    <span>{v?.nome}</span>
-                                 </div>
-                              </td>
-                              <td>{segundosParaFormatoHumanizado(v?.tempoTotalAbsoluto)}</td>
-                              <td>
-                                 <span className={`text-bg-${v?.nrTreinosRealizados > 0 ? "success" : "danger"} px-3 py-1 rounded`}>
-                                    {v?.nrTreinosRealizados}
-                                 </span>{" "}
-                              </td>
-                              <td>
-                                 <i className="bi bi-calendar2-date text-secondary me-1"></i> {new Date(v?.criadoEm).toLocaleDateString()}
-                              </td>
+                  <div id={styles.tableWrapper}>
+                     <Table striped className="mt-3 mt-lg-4 mt-xxl-5" bordered>
+                        <thead className="fst-italic">
+                           <tr>
+                              <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
+                                 <i className="bi bi-trophy-fill me-2"></i> Rank
+                              </th>
+                              <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
+                                 <i className="bi bi-person-fill me-2"></i> Usuário
+                              </th>
+                              <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
+                                 <i className="bi bi-clock-fill me-2"></i> Tempo de treino
+                              </th>
+                              <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
+                                 <i className="bi bi-person-arms-up me-2"></i> Treinos realizados
+                              </th>
+                              <th className={modoEscuro ? "text-bg-secondary" : `text-bg-dark`}>
+                                 <i className="bi bi-calendar-check me-2"></i> Cadastrado em
+                              </th>
                            </tr>
-                        ))}
-                     </tbody>
-                  </Table>
+                        </thead>
+                        <tbody>
+                           {usuarios?.map((v, k) => (
+                              <tr key={k}>
+                                 <td className="fst-italic fw-medium">{k + 1} º lugar</td>
+                                 <td>
+                                    <div className="d-flex align-items-center gap-3">
+                                       <Image id={styles.foto} className="rounded p-0" thumbnail src={v?.foto} />
+                                       <span className="text-truncate">{v?.nome}</span>
+                                    </div>
+                                 </td>
+                                 <td>{segundosParaFormatoHumanizado(v?.tempoTotalAbsoluto)}</td>
+                                 <td>
+                                    <span className={`text-bg-${v?.nrTreinosRealizados > 0 ? "success" : "danger"} px-3 py-1 rounded`}>
+                                       {v?.nrTreinosRealizados}
+                                    </span>{" "}
+                                 </td>
+                                 <td>
+                                    <i className="bi bi-calendar2-date text-secondary me-1"></i> {new Date(v?.criadoEm).toLocaleDateString()}
+                                 </td>
+                              </tr>
+                           ))}
+                        </tbody>
+                     </Table>
+                  </div>
                </Col>
             </Row>
          </div>
