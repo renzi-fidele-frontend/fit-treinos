8ba57e4e63eb271466617f7331eccb3827446689
Renzi Fidele <renzifidele2001@gmail.com>
2025-10-10 09:42:11 +0200

    fix: Corrigindo estilos das imagens de erro dos cards de estatísticas da dedicação semanal e exercício mais praticado

--- frontend/src/components/CardExercicioMaisTreinado/CardExercicioMaisTreinado.jsx
+++ frontend/src/components/CardExercicioMaisTreinado/CardExercicioMaisTreinado.jsx
@@ -26,7 +26,9 @@ const CardExercicioMaisTreinado = ({ exercicio, tempoDeTreino }) => {
 
    return (
       <div>
-         <h6 id={styles.tit} className="mb-3">{card6.stat}</h6>
+         <h6 id={styles.tit} className="mb-3">
+            {card6.stat}
+         </h6>
          <div>
             {exercicio && (
                <div>
@@ -59,34 +61,33 @@ const CardExercicioMaisTreinado = ({ exercicio, tempoDeTreino }) => {
                   <p className="fs-5 fw-semibold text-capitalize mt-2 text-truncate mb-0">{idioma?.includes("en") ? exercicio?.name : titulo}</p>
                </div>
             )}
-            {exercicio === null ||
-               (exercicio === undefined && (
-                  <div>
-                     <div to={`/exercicio/${exercicio?.id}`} className="position-relative h-100 d-flex justify-content-end align-items-end">
-                        <Placeholder animation="wave" xs={12}>
-                           <Placeholder className={styles.fotoLoad + " border rounded-1"} />
-                        </Placeholder>
-                        {/* Tag mobile */}
-                        <p className="text-capitalize text-bg-secondary px-2 py-1 rounded small mb-0 position-absolute d-xxl-none me-1 mb-1 shadow-lg">
+            {exercicio === null && (
+               <div>
+                  <div to={`/exercicio/${exercicio?.id}`} className="position-relative h-100 d-flex justify-content-end align-items-end">
+                     <Placeholder animation="wave" xs={12}>
+                        <Placeholder className={styles.fotoLoad + " border rounded-1"} />
+                     </Placeholder>
+                     {/* Tag mobile */}
+                     <p className="text-capitalize text-bg-secondary px-2 py-1 rounded small mb-0 position-absolute d-xxl-none me-1 mb-1 shadow-lg">
+                        <Placeholder>......</Placeholder>
+                     </p>
+                  </div>
+                  <div className="d-flex align-items-center mt-2">
+                     <div className="d-flex gap-2">
+                        {/* Tag desktop */}
+                        <p className="text-capitalize text-bg-secondary px-2 py-1 rounded small mb-0 d-none d-xxl-block ">
                            <Placeholder>......</Placeholder>
                         </p>
                      </div>
-                     <div className="d-flex align-items-center mt-2">
-                        <div className="d-flex gap-2">
-                           {/* Tag desktop */}
-                           <p className="text-capitalize text-bg-secondary px-2 py-1 rounded small mb-0 d-none d-xxl-block ">
-                              <Placeholder>......</Placeholder>
-                           </p>
-                        </div>
-                        <div className="vr mx-2 d-none d-xxl-block"></div>
-                        <p className="mb-0">
-                           {card4.tempo} <span className="fw-medium fst-italic border rounded px-1 shadow-sm">Carregando...</span>
-                        </p>
-                     </div>
-                     <p className="fs-5 fw-semibold text-capitalize mt-2 text-truncate mb-0">{exercicio?.name}</p>
+                     <div className="vr mx-2 d-none d-xxl-block"></div>
+                     <p className="mb-0">
+                        {card4.tempo} <span className="fw-medium fst-italic border rounded px-1 shadow-sm">Carregando...</span>
+                     </p>
                   </div>
-               ))}
-            {exercicio === false && (
+                  <p className="fs-5 fw-semibold text-capitalize mt-2 text-truncate mb-0">{exercicio?.name}</p>
+               </div>
+            )}
+            {exercicio === undefined && (
                <div className="d-flex flex-column gap-3 align-items-center justify-content-center h-100">
                   <Image src={noBestTrain} className={styles.noBestTrain} />
                   <p className="mb-0 tex-light bg-secondary-subtle px-3 py-1 rounded">{card5.noTrain}</p>
--- frontend/src/components/CardPartesDoCorpoMaisTreinadas/CardPartesDoCorpoMaisTreinadas.jsx
+++ frontend/src/components/CardPartesDoCorpoMaisTreinadas/CardPartesDoCorpoMaisTreinadas.jsx
@@ -19,34 +19,35 @@ const CardPartesDoCorpoMaisTreinadas = ({ partesDoCorpoTreinadas, centralizado }
          <h6 id={styles.tit} className={`mb-0 ${centralizado && "text-center"}`}>
             {card5.stat}
          </h6>
-         <div className="mt-2"></div>
-         {partesDoCorpoTreinadas ? (
-            partesDoCorpoTreinadas?.length > 0 ? (
-               <Pie
-                  data={{
-                     labels: partesDoCorpoTreinadas?.map((v) => (idioma?.includes("en") ? v?.nome : investigarParteDoCorpo(v?.nome))),
-                     datasets: [{ data: partesDoCorpoTreinadas?.map((v) => v?.tempoDeTreino) }],
-                  }}
-               />
+         <div className="mt-3">
+            {partesDoCorpoTreinadas ? (
+               partesDoCorpoTreinadas?.length > 0 ? (
+                  <Pie
+                     data={{
+                        labels: partesDoCorpoTreinadas?.map((v) => (idioma?.includes("en") ? v?.nome : investigarParteDoCorpo(v?.nome))),
+                        datasets: [{ data: partesDoCorpoTreinadas?.map((v) => v?.tempoDeTreino) }],
+                     }}
+                  />
+               ) : (
+                  <div className={styles.noProgress + " d-flex flex-column align-items-center w-100 gap-3 "}>
+                     <Image src={noProgress} />
+                     <p className="mb-0 tex-light bg-secondary-subtle px-3 py-1 rounded">{card5.noTrain}</p>
+                  </div>
+               )
             ) : (
-               <div className={styles.noProgress + " d-flex flex-column align-items-center w-100 gap-3 "}>
-                  <Image src={noProgress} />
-                  <p className="mb-0 tex-light bg-secondary-subtle px-3 py-1 rounded">{card5.noTrain}</p>
-               </div>
-            )
-         ) : (
-            <Placeholder animation="wave" className="" xs={12}>
-               <div className="mb-4 d-flex gap-2 justify-content-center flex-wrap">
-                  {gerarArray(10).map((v, k) => (
-                     <Placeholder xs={2} key={k} />
-                  ))}
-               </div>
-               <div className="d-flex align-items-center justify-content-center position-relative">
-                  <Placeholder className={styles.pieChartLoad} />
-                  <p className="mb-0 position-absolute">{card4.load}</p>
-               </div>
-            </Placeholder>
-         )}
+               <Placeholder animation="wave" className="" xs={12}>
+                  <div className="mb-4 d-flex gap-2 justify-content-center flex-wrap">
+                     {gerarArray(10).map((v, k) => (
+                        <Placeholder xs={2} key={k} />
+                     ))}
+                  </div>
+                  <div className="d-flex align-items-center justify-content-center position-relative">
+                     <Placeholder className={styles.pieChartLoad} />
+                     <p className="mb-0 position-absolute">{card4.load}</p>
+                  </div>
+               </Placeholder>
+            )}
+         </div>
       </div>
    );
 };
