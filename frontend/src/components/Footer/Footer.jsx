d947b287789d59a8305210a173bd21e225547095
Renzi Fidele <renzifidele2001@gmail.com>
2025-10-18 22:24:43 +0200

    fix: Corrigindo a responsividade do rodapé após se adicionar os links da políticas do site

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -11,12 +11,12 @@ const Footer = () => {
    const { t } = useTranslation();
    const { copy, dev, politica, terms, descricao } = t("footer");
    return (
-      <div className="text-bg-dark pt-3 pb-4">
+      <div className="text-bg-dark pt-3 pb-5 pb-xl-4 ">
          <Container>
             <hr />
             <Row>
-               <Col lg={8}>
-                  <div className="d-flex flex-column flex-lg-row align-items-center text-center text-lg-start gap-2 gap-lg-0">
+               <Col xl={8}>
+                  <div className="d-flex flex-column flex-xl-row align-items-center text-center text-lg-start gap-2 gap-lg-0">
                      <Link to="/">
                         <Image className="me-lg-3" src={logo} />
                      </Link>
@@ -24,13 +24,13 @@ const Footer = () => {
                         © 2025 - {copy}
                      </p>
                   </div>
-                  <p className="mb-0 mt-2">{descricao}</p>
+                  <p className="mb-0 mt-2 text-center text-xl-start">{descricao}</p>
                </Col>
-               <Col className="text-center text-lg-end mt-2 mt-lg-0">
+               <Col className="text-center text-xl-end mt-3 mt-xl-0">
                   <h2 className="fs-5 fw-bold">
                      {dev} <span className="fw-normal text-decoration-underline">Renzi Fidele</span>{" "}
                   </h2>
-                  <div className="d-flex gap-3 justify-content-end" id={styles.fim}>
+                  <div className="d-flex gap-3 justify-content-center justify-content-xl-end mt-3 mt-xl-0" id={styles.fim}>
                      <Link className="text-decoration-underline" to="/privacy">
                         {politica}
                      </Link>
