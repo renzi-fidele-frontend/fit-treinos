636549ba10d83bdc20f71f87720aed85d3d0250f
Renzi Fidele <renzifidele2001@gmail.com>
2025-11-25 13:12:19 +0200

    feat: Encontrando a localização do usuário (LAT e LONG) do usuário via geolocation

--- frontend/src/pages/Ginasios/Ginasios.jsx
+++ frontend/src/pages/Ginasios/Ginasios.jsx
@@ -7,8 +7,25 @@ import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import foto from "../../assets/findModel.webp";
 import ilustracao from "../../assets/FindIlustration.webp";
+import useFetch from "../../hooks/useFetch";
 
 const Ginasios = () => {
+   const { apanharDadosComParam } = useFetch();
+
+   // TODO: Integrar o places API para encontrar os ginásios mais próximos ao usuário
+   async function encontrarGinasios() {
+      const myLoc = navigator.geolocation.getCurrentPosition(
+         (position) => {
+            const lat = position.coords.latitude;
+            const long = position.coords.longitude;
+            console.log(`A latitude é ${lat} e a longitude é: ${long}`);
+         },
+         (error) => {
+            console.log("Erro ao obter a localização do usuário");
+         }
+      );
+   }
+
    return (
       <div>
          <BannerTopo
@@ -20,7 +37,7 @@ const Ginasios = () => {
             <Row className="py-4 py-sm-5 mb-sm-4">
                <Col className="d-flex flex-column align-items-center justify-content-center gap-4">
                   <Image className="rounded-3" src={ilustracao} alt="Ilustração demostrando a funcionalidade de busca por ginásios" />
-                  <Button size="lg" variant="secondary">
+                  <Button size="lg" variant="secondary" onClick={encontrarGinasios}>
                      <i className="bi bi-search me-1"></i> Iniciar busca
                   </Button>
                </Col>
