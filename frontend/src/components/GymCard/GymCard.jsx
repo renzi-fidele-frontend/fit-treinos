7a6cce93990c6acc93fb265399110c0261ca4616
Renzi Fidele <renzifidele2001@gmail.com>
2025-11-27 17:37:17 +0200

    feat: Adicionando o loading da foto dos ginásios próximos

--- frontend/src/components/GymCard/GymCard.jsx
+++ frontend/src/components/GymCard/GymCard.jsx
@@ -1,25 +1,28 @@
-import Image from "react-bootstrap/Image";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import styles from "./GymCard.module.css";
+import PreloadImage from "../ui/PreloadImage";
+import noGym from "../../assets/noGym.webp";
 
 const GymCard = ({ ginasio }) => {
    return (
       <Card className={"d-flex flex-row flex-nowrap"}>
-         <Image className={styles.cardImg} src={ginasio?.photos?.[0].getUrl()} />
+         <PreloadImage className={styles.cardImg} src={ginasio?.photos?.[0].getUrl()} errorSrc={noGym} preloaderCn={styles.cardImg} />
          <Card.Body>
             {/* Nome */}
             <h6 className="fs-6">{ginasio?.name}</h6>
             {/* Classificação */}
-            <p className="text-muted">
+            <p className="text-muted mb-1">
                {ginasio?.rating || 0} / 5.0 <i className="bi bi-star-fill text-warning me-2"></i> ({ginasio?.user_ratings_total || 0})
             </p>
             {/* Localização */}
             <p className={"text-truncate  " + styles.endereco}>
-               <i></i> {ginasio?.vicinity}
+               <i className="bi bi-geo-alt"></i> {ginasio?.vicinity}
             </p>
             {/* Contato via whatsapp */}
-            <Button size="sm" variant="success"><i className="bi bi-whatsapp me-1"></i> Entrar em contato</Button>
+            <Button size="sm" variant="success">
+               <i className="bi bi-whatsapp me-1"></i> Entrar em contato
+            </Button>
          </Card.Body>
       </Card>
    );
