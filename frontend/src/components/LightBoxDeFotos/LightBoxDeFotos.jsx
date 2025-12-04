a677dba8ea5dd8f825304083a0c8ce772edc6b45
Renzi Fidele <renzifidele2001@gmail.com>
2025-12-04 13:48:35 +0200

    fix: Recuperando o componente de LightBoxDeFotos para corrigir o problema de nomeclatura

---
+++ frontend/src/components/LightBoxDeFotos/LightBoxDeFotos.jsx
@@ -0,0 +1,20 @@
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+// Plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+const LightBoxDeFotos = ({ mostrar, fotos, onClose }) => {
+   return (
+      <Lightbox
+         close={onClose}
+         open={mostrar}
+         slides={fotos?.map((v) => ({ src: v?.getUrl() }))}
+         plugins={[Fullscreen, Slideshow, Zoom, Thumbnails]}
+      />
+   );
+};
+export default LightBoxDeFotos;
