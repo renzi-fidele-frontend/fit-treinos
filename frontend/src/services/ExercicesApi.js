ebe1aa0bc03c5c9704011237177ea712f483390d
Renzi Fidele <renzifidele2001@gmail.com>
2025-12-09 11:11:28 +0200

    fix: Atualizando a chave da API para a variável de ambiente correta

--- frontend/src/services/ExercicesApi.js
+++ frontend/src/services/ExercicesApi.js
@@ -1,7 +1,7 @@
 export const exercisesFetchOptions = {
    method: "GET",
    headers: {
-      "x-rapidapi-key": import.meta.env.VITE_EXERCISES_API_KEY,
+      "x-rapidapi-key": import.meta.env.VITE_EXERCISES_API_KEY2,
       "x-rapidapi-host": "exercisedb.p.rapidapi.com",
    },
 };
